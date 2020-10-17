3ed25b2910adac55aa13d27506a78eadb79ddb9c
TarasRohovets <Taras.Rohovets_extern@voith.com>
2020-10-18 00:41:05 +0100

    small changes

--- dex_app/src/contracts/DEX.sol
+++ dex_app/src/contracts/DEX.sol
@@ -3,26 +3,27 @@ pragma solidity 0.6.6;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract DEX {
+contract DecetralizedExchange {
     using SafeMath for uint256;
-    IERC20 token;
+    IERC20 generic_token;
 
     uint256 public totalLiquidity;
 
     mapping(address => uint256) public liquidity;
 
     constructor(address token_addr) public {
-        token = IERC20(token_addr);
+        generic_token = IERC20(token_addr);
     }
 
-    function init(uint256 tokens) public payable returns (uint256) {
-        require(totalLiquidity == 0, "DEX:init - alredy has liquidity");
+    function initialize(uint256 tokens) public payable returns (uint256) {
+        require(totalLiquidity == 0, "DecetralizedExchange - alredy has liquidity");
         totalLiquidity = address(this).balance;
         liquidity[msg.sender] = totalLiquidity;
-        require(token.transferFrom(msg.sender, address(this), tokens));
+        require(generic_token.transferFrom(msg.sender, address(this), tokens));
         return totalLiquidity;
     }
 
+    // like liquidity pool
     function price(
         uint256 input_amount, // how much we want
         uint256 input_reserve, // ETH reserves
@@ -34,50 +35,53 @@ contract DEX {
         return numerator / denominator;
     }
 
+    //swap
     function ethToToken() public payable returns (uint256) {
-        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 token_reserve = generic_token.balanceOf(address(this));
         uint256 tokens_bought = price(
             msg.value,
             address(this).balance.sub(msg.value),
             token_reserve
         );
-        require(token.transfer(msg.sender, tokens_bought));
+        require(generic_token.transfer(msg.sender, tokens_bought));
         return tokens_bought;
     }
 
+    //swap
     function tokenToEth(uint256 tokens) public returns (uint256) {
-        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 token_reserve = generic_token.balanceOf(address(this));
         uint256 eth_bought = price(
             tokens,
             token_reserve,
             address(this).balance
         );
         msg.sender.transfer(eth_bought);
-        require(token.transferFrom(msg.sender, address(this), tokens));
+        require(generic_token.transferFrom(msg.sender, address(this), tokens));
         return eth_bought;
     }
 
     function deposit() public payable returns (uint256) {
         uint256 eth_reserve = address(this).balance.sub(msg.value);
-        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 token_reserve = generic_token.balanceOf(address(this));
         uint256 token_amount = (msg.value.mul(token_reserve) / eth_reserve).add(
             1
         );
         uint256 liquidity_minted = msg.value.mul(totalLiquidity) / eth_reserve;
         liquidity[msg.sender] = liquidity[msg.sender].add(liquidity_minted);
         totalLiquidity = totalLiquidity.add(liquidity_minted);
-        require(token.transferFrom(msg.sender, address(this), token_amount));
+        require(generic_token.transferFrom(msg.sender, address(this), token_amount));
         return liquidity_minted;
     }
-
+     
+    
     function withdraw(uint256 amount) public returns (uint256, uint256) {
-        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 token_reserve = generic_token.balanceOf(address(this));
         uint256 eth_amount = amount.mul(address(this).balance) / totalLiquidity;
         uint256 token_amount = amount.mul(token_reserve) / totalLiquidity;
         liquidity[msg.sender] = liquidity[msg.sender].sub(eth_amount);
         totalLiquidity = totalLiquidity.sub(eth_amount);
         msg.sender.transfer(eth_amount);
-        require(token.tranfer(msg.sender, token_amount));
+        require(generic_token.tranfer(msg.sender, token_amount));
         return (eth_amount, token_amount)
     }
 }
