ef0b9a2f1224363d618e42ca33c03ae80a444950
TarasRohovets <Taras.Rohovets_extern@voith.com>
2020-10-18 00:32:17 +0100

    dex

--- dex_app/migrations/2_deploy_contracts.js
+++ dex_app/migrations/2_deploy_contracts.js
@@ -1,5 +1,19 @@
-const Migrations = artifacts.require("Migrations");
+const DEX = artifacts.require("DEX");
+const GenericToken = artifacts.require("GenericToken");
+const ethers = require('ethers');
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer) {
+  await deployer.deploy(GenericToken);
+  const genericToken = await GenericToken.deployed();
+
+  await deployer.deploy(DEX, genericToken.address);
+  const dex = await DEX.deployed();
+
+  genericToken.transfer("0x56D2F9807EF894Fa87A9b6A23227CE36156458CE", "" + (10 * 10 * 18));
+
+  await genericToken.approve(dex.address, ethers.utils.parseEther('100'));
+
+  await dex.init(ethers.utils.parseEther('5'));
+
+
 };
